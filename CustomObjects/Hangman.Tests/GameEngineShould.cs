d5d831efb5ad2d90bdd4ae3762977e224f85cb25
Yelena Gouralnik <you@example.com>
2020-12-16 11:25:35 -0700

    Custom Objects

---
+++ CustomObjects/Hangman.Tests/GameEngineShould.cs
@@ -0,0 +1,23 @@
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+using Hangman;
+using Xunit;
+
+namespace Tests
+{
+    public class GameEngineShould
+    {
+        [Fact]
+        [UseReporter(typeof(DiffReporter))]
+        public void UpdateGameState()
+        {
+            var sut = new GameEngine("Pluralsight");
+
+            sut.Guess('x');
+            sut.Guess('p');
+            sut.Guess('l');
+
+            Approvals.Verify(sut);
+        }
+    }
+}
---
+++ CustomObjects/Hangman/GameEngine.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Hangman
+{
+    public class GameEngine
+    {
+        private readonly string _wordToGuess;
+
+        public GameEngine(string wordToGuess)
+        {
+            _wordToGuess = wordToGuess.ToUpperInvariant();
+        }
+
+        public List<char> GuessedLetters { get; } = new List<char>();
+
+        public int GuessesRemaining { get; private set; } = 10;
+
+        public string RevealedWord
+        {
+            get
+            {
+                var sb = new StringBuilder();
+
+                foreach (var letter in _wordToGuess.ToCharArray())
+                {
+                    if (GuessedLetters.Contains(letter))
+                    {
+                        sb.Append(letter);
+                    }
+                    else
+                    {
+                        sb.Append('-');
+                    }
+                }
+
+                return sb.ToString();
+            }
+        }        
+
+        public void Guess(char letter)
+        {
+            letter = ToUpperChar(letter);
+
+            GuessedLetters.Add(letter);
+
+            var incorrectGuess = !_wordToGuess.ToCharArray().Any(x => x == letter);
+
+            if (incorrectGuess)
+            {
+                GuessesRemaining--;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendFormat("Guessed letters: {0}", new string(GuessedLetters.ToArray()));
+            sb.AppendLine();
+
+            sb.AppendFormat("Guesses remaining: {0}", GuessesRemaining);
+            sb.AppendLine();
+
+            sb.Append(RevealedWord);
+
+            return sb.ToString();
+        }
+
+        #region helper code
+        private static char ToUpperChar(char letter)
+        {
+            return letter.ToString(CultureInfo.InvariantCulture).ToUpperInvariant().ToCharArray()[0];
+        }
+        #endregion
+    }
+}
