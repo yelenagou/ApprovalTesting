961c6e4113fab7184e8cbc9d240005bd92899535
Yelena Gouralnik <you@example.com>
2020-12-17 12:58:38 -0700

    adding scrubbing project

---
+++ Scrubbing/HRReportGenerator/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HRReportGenerator
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PayGrade { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
